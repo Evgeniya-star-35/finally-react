95e9659a4c41141407db7419728c7220f0416f20
Evgeniya Bayda <baje75128@gmail.com>
2022-02-03 19:09:31 +0200

    fix fonts

--- src/components/Container/Container.jsx
+++ src/components/Container/Container.jsx
@@ -2,10 +2,10 @@ import s from './Container.module.css';
 import PropTypes from 'prop-types';
 
 function Container({ children }) {
-  return <div className={s.Container}>{children}</div>;
+    return <div className={s.Container}>{children}</div>;
 }
 
 Container.propTypes = {
-  children: PropTypes.node,
+    children: PropTypes.node,
 };
 export default Container;
