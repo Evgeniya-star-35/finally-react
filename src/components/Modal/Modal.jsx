d4a150281e6a071e253542b07f9b2b26af8fbaa7
Jane Yushchenko <kelenella@ukr.net>
2022-02-04 18:29:10 +0200

    fixed logout

---
+++ src/components/Modal/Modal.jsx
@@ -0,0 +1,9 @@
+import { createPortal } from 'react-dom';
+import PropTypes from 'prop-types';
+import s from './Modal.module.css';
+
+const modalRoot = document.querySelector('#modalRoot');
+
+export default function ModalLogout({ children }) {
+    return <></>;
+}
