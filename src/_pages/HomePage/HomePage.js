967f4c575318d83eacdae8c76e18b37bb1233e3b
MariiaMelychyn <lv080191mmi75@gmail.com>
2022-02-05 14:27:24 +0200

    forma

--- src/_pages/HomePage/HomePage.js
+++ src/_pages/HomePage/HomePage.js
@@ -1,13 +1,14 @@
 import BackgroundHome from 'components/BackgroundHome/BackgroundHome';
 import AuthHeader from 'components/Header/AuthHeader';
 import Header from 'components/Header/Header';
-
+import RegisterForm from 'components/RegisterForm';
 const HomePage = () => {
     return (
         <>
             <Header />
             <AuthHeader />
             <BackgroundHome />
+            <RegisterForm />
         </>
     );
 };
