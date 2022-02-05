24bcd1974c2ef7ca5a2ace335680775f07470f43
MariiaMelychyn <lv080191mmi75@gmail.com>
2022-02-06 01:12:17 +0200

    registration

--- src/_pages/HomePage/HomePage.js
+++ src/_pages/HomePage/HomePage.js
@@ -2,13 +2,14 @@ import BackgroundHome from 'components/BackgroundHome/BackgroundHome';
 import AuthHeader from 'components/Header/AuthHeader';
 import Header from 'components/Header/Header';
 import TransactionForm from 'components/TransactionForm';
-
+import RegisterForm from 'components/RegisterForm/RegisterForm';
 const HomePage = () => {
     return (
         <>
             <Header />
             <AuthHeader />
             <BackgroundHome />
+            <RegisterForm />
             <TransactionForm />
         </>
     );
--- src/components/RegisterForm/RegisterForm.jsx
+++ src/components/RegisterForm/RegisterForm.jsx
@@ -1,66 +1,102 @@
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { register } from '../../redux/auth';
 import Button from '../Buttons/Button';
-import s from './RegisterForm.module.css';
+import styles from './RegisterForm.module.css';
 
 export default function RegisterForm() {
+    const dispatch = useDispatch();
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [required, setRequired] = useState(false);
+
+    const onLogIn = () => dispatch(register.logIn({ email, password }));
+
+    const handleChange = ({ target: { name, value } }) => {
+        switch (name) {
+            case 'email':
+                return setEmail(value);
+            case 'password':
+                return setPassword(value);
+            default:
+                return;
+        }
+    };
+
+    const reset = () => {
+        setEmail('');
+        setPassword('');
+    };
+
+    const handleSubmit = e => {
+        e.preventDefault();
+
+        if (email === '' || password === '') {
+            setRequired(true);
+            return;
+        } else {
+            setRequired(false);
+        }
+        onLogIn();
+        reset();
+    };
+
     return (
-        <div className={s.FormContent}>
-            <p className={s.FormContent_text}>
+        <form onSubmit={handleSubmit} className={styles.form} autoComplete="on">
+            <p className={styles.textGoogle}>
                 Вы можете авторизоваться с помощью Google Account:
             </p>
-            <a
-                className={s.FormContent_button}
-                href="https://finally-node.herokuapp.com/api/auth/google"
-            >
-                <span></span>
-                Google
-            </a>
-            <p className={s.FormContent_textA}>
+            <div>
+                <a
+                    className={styles.FormContent_button}
+                    href="https://finally-node.herokuapp.com/api/auth/google"
+                >
+                    <span></span>
+                    Google
+                </a>
+            </div>
+            <p className={styles.text}>
                 Или зайти с помощью e-mail и пароля, предварительно
                 зарегистрировавшись:
             </p>
-            <div>
-                <div className={s.AuthNav_wrapper}>
-                    <form className="AuthNav_form" autocomplete="off">
-                        <label className={s.AuthNav_label__2}>
-                            <p className={s.AuthNav_description}>Имя</p>
-                            <input
-                                className={s.AuthNav_input}
-                                type="text"
-                                name="name"
-                                required
-                                value
-                            ></input>
-                        </label>
-                        <label className={s.AuthNav_label__3}>
-                            <p className={s.AuthNav_description}>
-                                Электронная почта
-                            </p>
-                            <input
-                                className={s.AuthNav_input}
-                                type="email"
-                                name="email"
-                                required
-                                value
-                            ></input>
-                        </label>
-                        <label className={s.AuthNav_label__4}>
-                            <p className={s.AuthNav_description}>Пароль: </p>
-                            <input
-                                className={s.AuthNav_input}
-                                placeholder="более 8 символов"
-                                type="password"
-                                name="password"
-                                required
-                                value
-                            ></input>
-                        </label>
-                        <div>
-                            <Button text={'войти'} type="submit" />
-                            <Button text={'регистрация'} type="button" />
-                        </div>
-                    </form>
-                </div>
+            <label className={styles.label}>
+                {required && <span className={styles.span_star}>*</span>}
+                Электронная почта:
+                <input
+                    className={styles.input}
+                    type="email"
+                    name="email"
+                    value={email}
+                    placeholder="your@email.com"
+                    onChange={handleChange}
+                />
+                {required && (
+                    <span className={styles.span_required}>
+                        это обязательное поле
+                    </span>
+                )}
+            </label>
+            <label className={styles.label}>
+                {required && <span className={styles.span_star}>*</span>}
+                Пароль:
+                <input
+                    className={styles.input}
+                    type="password"
+                    name="password"
+                    value={password}
+                    placeholder="Пароль"
+                    onChange={handleChange}
+                />
+                {required && (
+                    <span className={styles.span_required}>
+                        это обязательное поле
+                    </span>
+                )}
+            </label>
+            <div className={styles.button__container}>
+                <Button text={'войти'} type="submit" />
+                <Button text={'регистрация'} type="button" />
             </div>
-        </div>
+        </form>
     );
 }
