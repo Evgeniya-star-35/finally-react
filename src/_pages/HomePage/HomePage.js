2381ca3941996d03070b2743eebe0a090dc31be3
Evgeniya Bayda <baje75128@gmail.com>
2022-02-06 22:39:03 +0200

    fix body

--- src/_pages/HomePage/HomePage.js
+++ src/_pages/HomePage/HomePage.js
@@ -1,14 +1,7 @@
-
-import BackgroundHome from 'components/BackgroundHome/BackgroundHome';
-import AuthHeader from 'components/Header/AuthHeader';
-import Header from 'components/Header/Header';
 import RegisterForm from 'components/RegisterForm';
 import CategoriesCosts from 'components/CategoriesCosts';
 import CategoriesIncomes from 'components/CategoriesIncomes';
 import TransactionForm from 'components/TransactionForm';
-
-
-
 
 const HomePage = () => {
     return (
@@ -19,7 +12,6 @@ const HomePage = () => {
             <CategoriesIncomes />
 
             <TransactionForm />
-
         </>
     );
 };
