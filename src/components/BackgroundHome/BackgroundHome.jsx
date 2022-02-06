d923002ba63af743697f08a6090a3fee65915645
marta <vynnytska1989@gmail.com>
2022-02-06 14:08:01 +0200

    fix

--- src/components/BackgroundHome/BackgroundHome.jsx
+++ src/components/BackgroundHome/BackgroundHome.jsx
@@ -2,6 +2,7 @@ import s from './BackgroundHome.module.css';
 const BackgroundHome = ({ children }) => {
     return (
         <>
+            {/* old */}
             {/* <div className={s.Container}> */}
             {/* <div className={s.Topbox}>
                 <div className={s.TopImg}></div>
@@ -9,6 +10,7 @@ const BackgroundHome = ({ children }) => {
             </div>
                 <div className={s.BottomImg}></div> */}
             {/* </div> */}
+
             <div className={s.BottomImg}>
                 <div className={s.Topbox}></div>
                 <div className={s.TopImg}></div>
