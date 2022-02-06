44c7b66eb6f8ffb71ec69fe623e6cd44e569afc8
marta <vynnytska1989@gmail.com>
2022-02-06 15:29:00 +0200

    fix background

--- src/_pages/HomePage/HomePage.js
+++ src/_pages/HomePage/HomePage.js
@@ -1,19 +1,20 @@
 // import BackgroundHome from 'components/BackgroundHome/BackgroundHome';
 
-import AuthHeader from 'components/Header/AuthHeader';
-import Header from 'components/Header/Header';
+// import AuthHeader from 'components/Header/AuthHeader';
+// import Header from 'components/Header/Header';
 import RegisterForm from 'components/RegisterForm/RegisterForm';
 
-import TransactionForm from 'components/TransactionForm';
+// import TransactionForm from 'components/TransactionForm';
 
 const HomePage = () => {
     return (
         <>
-            {/* <BackgroundHome /> */}
+            {/* <BackgroundHome > */}
 
             <RegisterForm />
 
-            <TransactionForm />
+            {/* <TransactionForm /> */}
+            {/* </BackgroundHome> */}
         </>
     );
 };
