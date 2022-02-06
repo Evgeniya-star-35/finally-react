a1ce72939cc647139af1c93bed607ed911dfeaf1
MariiaMelychyn <lv080191mmi75@gmail.com>
2022-02-07 00:36:44 +0200

    remove

--- src/components/RegisterForm/RegisterForm.jsx
+++ src/components/RegisterForm/RegisterForm.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Button from '../Buttons/Button';
 import styles from './RegisterForm.module.css';
 import { Formik, Field, Form } from 'formik';
