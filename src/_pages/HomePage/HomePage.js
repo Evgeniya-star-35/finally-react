531433e403e8bda0808964e430371fbd52cec4a9
Jane Yushchenko <kelenella@ukr.net>
2022-02-07 12:49:21 +0200

    svodka-styles

--- src/_pages/HomePage/HomePage.js
+++ src/_pages/HomePage/HomePage.js
@@ -2,11 +2,12 @@ import RegisterForm from 'components/RegisterForm';
 import CategoriesCosts from 'components/CategoriesCosts';
 import CategoriesIncomes from 'components/CategoriesIncomes';
 import TransactionForm from 'components/TransactionForm';
+import Summary from 'components/Summary';
 const HomePage = () => {
     return (
         <>
             <RegisterForm />
-
+            <Summary />
             <CategoriesCosts />
             <CategoriesIncomes />
             <TransactionForm />
