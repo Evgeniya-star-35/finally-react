2a264b753eadd9fad702a7c049a97749b4d0864e
tanyamelnyk19 <79224689+tanyamelnyk19@users.noreply.github.com>
2022-02-07 12:34:46 +0200

    add svg hover & button 'go to reports'

--- src/_pages/HomePage/HomePage.js
+++ src/_pages/HomePage/HomePage.js
@@ -1,15 +1,18 @@
-import RegisterForm from 'components/RegisterForm';
+// import RegisterForm from 'components/RegisterForm';
 import CategoriesCosts from 'components/CategoriesCosts';
 import CategoriesIncomes from 'components/CategoriesIncomes';
 import TransactionForm from 'components/TransactionForm';
+import GoToReports from 'components/GoToReports';
 
 const HomePage = () => {
     return (
         <>
-            <RegisterForm />
+            {/* <RegisterForm /> */}
 
             <CategoriesCosts />
             <CategoriesIncomes />
+
+            <GoToReports />
 
             <TransactionForm />
         </>
