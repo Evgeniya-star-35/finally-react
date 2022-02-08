5bc7d0781dc1e59ce9f37fa4ed3cbf33344e125d
Jane Yushchenko <kelenella@ukr.net>
2022-02-09 00:33:54 +0200

    fixed

--- src/_pages/HomePage/HomePage.js
+++ src/_pages/HomePage/HomePage.js
@@ -1,18 +1,14 @@
 import Header from 'components/Header/Header';
 import BackgroundHome from '../../components/BackgroundHome';
-// import RegisterForm from 'components/RegisterForm';
-import TransactionForm from 'components/TransactionForm';
-
+import RegisterForm from 'components/RegisterForm';
 
 const HomePage = () => {
     return (
         <>
             <BackgroundHome>
                 <Header />
-                {/* <RegisterForm /> */}
-                <TransactionForm />
+                <RegisterForm />
             </BackgroundHome>
-
         </>
     );
 };
