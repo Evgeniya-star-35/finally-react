a0c1a1240d61b6a444f0ebde1e15e4c883d41e57
Jane Yushchenko <kelenella@ukr.net>
2022-02-09 01:54:40 +0200

    поправила верстку мейн пейдж

--- src/components/TransactionTable/TransactionTable.jsx
+++ src/components/TransactionTable/TransactionTable.jsx
@@ -8,67 +8,81 @@ export default function TransactionTable() {
             <Media
                 query="(min-width: 768px)"
                 render={() => (
-                    <table className={s.transactionTable}>
-                        <thead className={s.thead}>
-                            <tr>
-                                <th className={`${s.name} ${s.date}`}>Дата</th>
-                                <th className={`${s.name} ${s.description}`}>
-                                    Описание
-                                </th>
-                                <th className={`${s.name} ${s.category}`}>
-                                    Категория
-                                </th>
-                                <th className={`${s.name} ${s.sum}`}>Сумма</th>
-                                <th className={`${s.name} ${s.icon}`}></th>
-                            </tr>
-                        </thead>
+                    <div className={s.tableWrapper}>
+                        <table className={s.transactionTable}>
+                            <thead className={s.thead}>
+                                <tr>
+                                    <th className={`${s.name} ${s.date}`}>
+                                        Дата
+                                    </th>
+                                    <th
+                                        className={`${s.name} ${s.description}`}
+                                    >
+                                        Описание
+                                    </th>
+                                    <th className={`${s.name} ${s.category}`}>
+                                        Категория
+                                    </th>
+                                    <th className={`${s.name} ${s.sum}`}>
+                                        Сумма
+                                    </th>
+                                    <th className={`${s.name} ${s.icon}`}></th>
+                                </tr>
+                            </thead>
 
-                        <tbody className={s.tbody}>
-                            {/* {items.map(({ id, date, description, category, sum }) => ( */}
-                            <tr className={s.tableRow}>
-                                <td className={s.date}>05.09.2019</td>
-                                <td className={s.description}>Бананы</td>
-                                <td className={s.category}>Продукты</td>
-                                <td className={s.sum}>- 50.00 грн.</td>
-                                <td className={s.icon}>
-                                    <button type="button" className={s.button}>
-                                        <svg
-                                            width="18"
-                                            height="18"
-                                            className={s.basket}
+                            <tbody className={s.tbody}>
+                                {/* {items.map(({ id, date, description, category, sum }) => ( */}
+                                <tr className={s.tableRow}>
+                                    <td className={s.date}>05.09.2019</td>
+                                    <td className={s.description}>Бананы</td>
+                                    <td className={s.category}>Продукты</td>
+                                    <td className={s.sum}>- 50.00 грн.</td>
+                                    <td className={s.icon}>
+                                        <button
+                                            type="button"
+                                            className={s.button}
                                         >
-                                            <use
-                                                href={`${spriteGlobal}#icon-delete`}
-                                            ></use>
-                                        </svg>
-                                    </button>
-                                </td>
-                            </tr>
+                                            <svg
+                                                width="18"
+                                                height="18"
+                                                className={s.basket}
+                                            >
+                                                <use
+                                                    href={`${spriteGlobal}#icon-delete`}
+                                                ></use>
+                                            </svg>
+                                        </button>
+                                    </td>
+                                </tr>
 
-                            <tr className={s.tableRow}>
-                                <td className={s.date}>05.09.2019</td>
-                                <td className={s.description}>
-                                    Lorem ipsum dolor sit amet consectetur
-                                    adipisicing elit.
-                                </td>
-                                <td className={s.category}>Продукты</td>
-                                <td className={s.sum}>- 1050.00 грн.</td>
-                                <td className={s.icon}>
-                                    <button type="button" className={s.button}>
-                                        <svg
-                                            width="18"
-                                            height="18"
-                                            className={s.basket}
+                                <tr className={s.tableRow}>
+                                    <td className={s.date}>05.09.2019</td>
+                                    <td className={s.description}>
+                                        Lorem ipsum dolor sit amet consectetur
+                                        adipisicing elit.
+                                    </td>
+                                    <td className={s.category}>Продукты</td>
+                                    <td className={s.sum}>- 1050.00 грн.</td>
+                                    <td className={s.icon}>
+                                        <button
+                                            type="button"
+                                            className={s.button}
                                         >
-                                            <use
-                                                href={`${spriteGlobal}#icon-delete`}
-                                            ></use>
-                                        </svg>
-                                    </button>
-                                </td>
-                            </tr>
-                        </tbody>
-                    </table>
+                                            <svg
+                                                width="18"
+                                                height="18"
+                                                className={s.basket}
+                                            >
+                                                <use
+                                                    href={`${spriteGlobal}#icon-delete`}
+                                                ></use>
+                                            </svg>
+                                        </button>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
                 )}
             />
         </>
