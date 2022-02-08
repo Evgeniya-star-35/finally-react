dbb841c2c4311b7acef0bc6289ab69479cb319d2
Jane Yushchenko <kelenella@ukr.net>
2022-02-08 13:43:56 +0200

    add logik to GoToMainButton

--- src/components/Buttons/GoToMainButton/GoToMainButton.jsx
+++ src/components/Buttons/GoToMainButton/GoToMainButton.jsx
@@ -1,21 +1,34 @@
+import { Link } from 'react-router-dom';
+import { useLocation } from 'react-router';
 import Media from 'react-media';
 import sprite from '../../../images/globalIcons/symbol-defs.svg';
 import s from './GoToMainButton.module.css';
 
 export default function GoToMainButton() {
+    const location = useLocation();
     return (
         <>
             <Media
                 query="(min-width: 768px)"
                 render={() => (
                     <>
-                        <button className={s.btn} onClick={''}>
-                            <svg width="24" height="24" className={s.svg}>
-                                <use
-                                    href={`${sprite}#icon-keyboard_backspace-24px-1`}
-                                ></use>
-                            </svg>
-                            Вернуться на главную
+                        <button className={s.btn}>
+                            <Link
+                                to={{
+                                    pathname: '/mainPage',
+                                    state: {
+                                        from: { location },
+                                    },
+                                }}
+                                className={s.link}
+                            >
+                                <svg width="24" height="24" className={s.svg}>
+                                    <use
+                                        href={`${sprite}#icon-keyboard_backspace-24px-1`}
+                                    ></use>
+                                </svg>
+                                Вернуться на главную
+                            </Link>
                         </button>
                     </>
                 )}
