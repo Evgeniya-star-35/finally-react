ac65d67c271b98170098ca88da9e2f35d39a5224
marta <vynnytska1989@gmail.com>
2022-02-10 10:55:25 +0200

    fix balance

--- src/_pages/HomePage/HomePage.js
+++ src/_pages/HomePage/HomePage.js
@@ -1,7 +1,7 @@
 import Header from 'components/Header/Header';
 // import BackgroundHome from '../../components/BackgroundHome';
 import RegisterForm from 'components/RegisterForm';
-// import Title from 'components/Title';
+import Title from 'components/Title';
 // import TransactionForm from 'components/TransactionForm';
 
 const HomePage = () => {
@@ -9,6 +9,7 @@ const HomePage = () => {
         <>
             {/* <BackgroundHome> */}
             <Header />
+            <Title />
             <RegisterForm />
             {/* </BackgroundHome> */}
         </>
