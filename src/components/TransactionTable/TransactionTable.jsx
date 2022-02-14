80ff6389451c38b06e202c2bcc43e2b5912d4d30
Evgeniya Bayda <baje75128@gmail.com>
2022-02-14 09:27:42 +0200

    fix conflict

--- src/components/TransactionTable/TransactionTable.jsx
+++ src/components/TransactionTable/TransactionTable.jsx
@@ -169,7 +169,7 @@ export default function TransactionTable({
                                                             handleDeleteClick(
                                                                 id,
                                                             )
-
+                                                        }
                                                     >
                                                         <svg
                                                             width="18"
